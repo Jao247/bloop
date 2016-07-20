3b72607250e69fe0202882fd3e5cb7000649e233
Jason-Fahy <jason.fahy@cgatechnology.com>
2016-07-20 09:11:03 +0100

    commit w/o xcode data

--- ConnectEx/DataVC.swift
+++ ConnectEx/DataVC.swift
@@ -1,23 +1,16 @@
 import UIKit
 
-@IBObject public class ViewController1: UIViewController {
+@IBObject public class DataVC: UIViewController {
 		
 	public init() {
-		
 		super.init()
 	}
 		
 	public override func viewDidLoad() {
-	
 		super.viewDidLoad()
-
-		// Do any additional setup after loading the view.
 	}
 
 	public override func didReceiveMemoryWarning() {
-
 		super.didReceiveMemoryWarning()
-
-		// Dispose of any resources that can be recreated.
 	}
 }
